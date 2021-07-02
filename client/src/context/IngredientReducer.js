ec64471afbe2d48d3ce13896d054914430119b73
Nishant <nishantbhosale244@gmail.com>
2021-07-02 17:49:07 +0530

    Added add and delete feature

--- client/src/context/IngredientReducer.js
+++ client/src/context/IngredientReducer.js
@@ -1,11 +1,18 @@
+import {
+	ADD_INGREDIENTS,
+	SET_ALL_INGREDIENTS,
+	FILTER_INGREDIENTS,
+	REMOVE_INGREDIENTS,
+} from "./types";
+
 const reducer = (state, action) => {
 	switch (action.type) {
-		case "SET_INGREDIENTS":
+		case SET_ALL_INGREDIENTS:
 			return {
 				...state,
 				filtered: state.ingredients,
 			};
-		case "FILTER_INGREDIENTS":
+		case FILTER_INGREDIENTS:
 			return {
 				...state,
 				//eslint-disable-next-line
--- client/src/context/IngredientState.js
+++ client/src/context/IngredientState.js
@@ -2,6 +2,12 @@ import { useReducer } from "react";
 // import axios from "axios";
 import ingredientContext from "./IngredientContext";
 import ingredientReducer from "./IngredientReducer";
+import {
+	ADD_INGREDIENTS,
+	SET_ALL_INGREDIENTS,
+	FILTER_INGREDIENTS,
+	REMOVE_INGREDIENTS,
+} from "./types";
 
 const IngredientState = (props) => {
 	const initialState = {
@@ -9,6 +15,7 @@ const IngredientState = (props) => {
 			{
 				name: "Vegetable Pizza",
 				desc: "Capsicum, Onion, Tomato, Beans, Carrot, Flower, Cheese",
+				id: 1,
 				image:
 					"https://images.pexels.com/photos/4394612/pexels-photo-4394612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
 				price: {
@@ -20,40 +27,54 @@ const IngredientState = (props) => {
 			{
 				name: "Aloo Tikki",
 				price: 70,
+				id: 2,
 				category: "Sandwich",
 			},
 			{
 				name: "Chicken Burger",
 				price: 90,
+				id: 3,
 				category: "Burger",
 			},
 			{
 				name: "Paneer Momos",
 				price: 100,
+				id: 4,
 				category: "Momos",
 			},
 			{
 				name: "Peri-Peri",
 				price: 70,
+				id: 5,
 				category: "Fries",
 			},
 			{
 				name: "Chicken nuggets (6 pieces)",
 				price: 100,
+				id: 6,
 				category: "Snacks",
 			},
 		],
 		filtered: [],
+		userIngredients: [],
 	};
 
 	const [state, dispatch] = useReducer(ingredientReducer, initialState);
 
 	const setAllIngredients = () => {
-		dispatch({ type: "SET_INGREDIENTS" });
+		dispatch({ type: SET_ALL_INGREDIENTS });
 	};
 
 	const filterIngredients = (category) => {
-		dispatch({ type: "FILTER_INGREDIENTS", payload: category });
+		dispatch({ type: FILTER_INGREDIENTS, payload: category });
+	};
+
+	const addIngredients = () => {
+		dispatch({ type: ADD_INGREDIENTS });
+	};
+
+	const removeIngredients = () => {
+		dispatch({ type: REMOVE_INGREDIENTS });
 	};
 
 	return (
@@ -63,6 +84,8 @@ const IngredientState = (props) => {
 				filtered: state.filtered,
 				filterIngredients,
 				setAllIngredients,
+				addIngredients,
+				removeIngredients,
 			}}
 		>
 			{props.children}
